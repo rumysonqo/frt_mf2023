01e9e295c071029ac41f61b3820465fa0a63a223
abel laurente <abellaurente@hotmail.com>
2023-04-07 15:29:56 -0500

    segundo

--- src/pages/InicioPage.vue
+++ src/pages/InicioPage.vue
@@ -1,8 +1,7 @@
 <template>
   <q-page>
-    <div class="q-pa-md shadow-5">
-      <div class="row">
-        <div class="col">
+    <div class="q-pa-md q-gutter-sm shadow-5">
+        <div class="col-12">
           <q-select
             outlined
             v-model="cod_prg"
@@ -11,14 +10,12 @@
             option-label="programa"
             option-value="cod_programa"
             label="Programas"
-            style="min-width: 500px; max-width: 600px"
+            style="min-width: 800px; max-width: 1200px"
             @update:model-value="get_productos()"
           />
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col">
+        <div class="col-12">
           <q-select
             outlined
             v-model="cod_prd"
@@ -26,29 +23,12 @@
             option-label="producto"
             option-value="cod_producto"
             label="Productos"
-            style="min-width: 500px; max-width: 600px"
-            @update:model-value="get_actividades()"
-          />
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <q-select
-            outlined
-            v-model="cod_act"
-            :options="ds_actividades"
-            option-label="actividad"
-            option-value="cod_actividad"
-            label="Actividades"
-            style="min-width: 500px; max-width: 600px"
+            style="min-width: 800px; max-width: 1200px"
             @update:model-value="get_sub_productos()"
           />
         </div>
-      </div>
 
-      <div class="row">
-        <div class="col">
+        <div class="col-12">
           <q-select
             outlined
             v-model="cod_sub"
@@ -56,33 +36,79 @@
             option-label="sub_finalidad"
             option-value="cod_subfinalidad"
             label="Sub Productos"
-            style="min-width: 500px; max-width: 600px"
-            @update:model-value="get_rep_prg_fte()"
+            style="min-width: 800px; max-width: 1200px"
+            @update:model-value="get_metas_subpro()"
           />
         </div>
-      </div>
+
     </div>
+
+    <div class="q-pa-md q-gutter-sm shadow-5">
+      <q-table
+      title="Metas Fisicas 2023"
+      :rows="ds_metas"
+      :columns="ds_titulo"
+      row-key="name"
+      no-data-label="sin datos que mostrar"
+      :separator="sepa"
+      :filter="filter"
+      bordered
+      square
+      title-class="text-bold text-blue-9"
+      table-class="bg-grey-2 text-blue-10"
+      table-style="border: 1px solid text-blue-10;"
+      table-header-style="height: 65px;"
+      table-header-class="bg-blue-9 text-white"
+      :pagination="initialPagination"
+    >
+      <template v-slot:top-left>
+        <q-input outlined dense debounce="600" v-model="filter" placeholder="Buscar" style="min-width: 450px; max-width: 600px">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
+  </div>
+
   </q-page>
 </template>
 
 <script>
+import { ref } from 'vue'
 import axios from "axios";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "InicioPage",
   setup() {
-    return {};
+    return {
+      initialPagination: {
+      sortBy: 'desc',
+      descending: false,
+      page: 1,
+      rowsPerPage: 10,
+      }
+
+    };
   },
   mounted() {
     this.get_programas();
   },
   data() {
     return {
+      sepa:'cell',
       ds_programas: [],
       ds_productos: [],
       ds_actividades: [],
       ds_subpro: [],
+      ds_metas:[],
+      filter:'',
+      ds_titulo: [
+      { name: 'MICRO_RED', align: 'left', label: 'MICRO RED', field: 'nom_micro_red', sortable: true, headerStyle: {fontSize: '1.2em'},style: {fontSize: '1em'}},
+      { name: 'EESS', align: 'left', label: 'ESTABLECIMIENTO', field: 'nom_establecimiento', sortable: true,headerStyle: {fontSize: '1.2em'},style: {fontSize: '1em'} },
+      { name: 'META', align: 'right', label: 'META FISICA', field: 'meta_fisica', sortable: true,headerStyle: {fontSize: '1.2em'},style: {fontSize: '1em'} },
+      ],
       cod_prg: null,
       cod_prd: null,
       cod_act: null,
@@ -91,7 +117,7 @@ export default defineComponent({
   },
   methods: {
     async get_programas() {
-      let url = "http://localhost:3000/programas";
+      let url = "http://localhost:3000/api/programas";
       try {
         let datos = await axios.get(url);
         this.ds_programas = await datos.data;
@@ -101,7 +127,7 @@ export default defineComponent({
     },
 
     async get_productos() {
-      let url = "http://localhost:3000/productos";
+      let url = "http://localhost:3000/api/productos";
       try {
         let datos = await axios.get(url + "/" + this.cod_prg["cod_programa"]);
         this.ds_productos = await datos.data;
@@ -111,7 +137,7 @@ export default defineComponent({
     },
 
     async get_actividades() {
-      let url = "http://localhost:3000/actividades";
+      let url = "http://localhost:3000/api/actividades";
       try {
         let datos = await axios.get(
           url +
@@ -127,22 +153,38 @@ export default defineComponent({
     },
 
     async get_sub_productos() {
-      let url = "http://localhost:3000/sub_productos";
+      let url = "http://localhost:3000/api/sub_productos";
       try {
         let datos = await axios.get(
           url +
             "/" +
             this.cod_prg["cod_programa"] +
             "/" +
-            this.cod_prd["cod_producto"] +
-            "/" +
-            this.cod_act["cod_actividad"]
+            this.cod_prd["cod_producto"] 
         );
         this.ds_subpro = await datos.data;
       } catch (error) {
         console.log(error);
       }
     },
+
+    async get_metas_subpro() {
+      let url = "http://localhost:3000/api/metas_subpro";
+      try {
+        let datos = await axios.get(
+          url +
+            "/" +
+            this.cod_prg["cod_programa"] +
+            "/" +
+            this.cod_prd["cod_producto"]+
+            "/" +
+            this.cod_sub["cod_subfinalidad"]
+        );
+        this.ds_metas = await datos.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
 });
 </script>
