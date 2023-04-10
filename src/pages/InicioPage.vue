6bd75ea38044c9fb2c6c3101414c6c45f24c7e7a
abel laurente <abellaurente@hotmail.com>
2023-04-10 16:00:33 -0500

    cuarto

--- src/pages/InicioPage.vue
+++ src/pages/InicioPage.vue
@@ -155,21 +155,6 @@ export default defineComponent({
       cod_prd: null,
       cod_act: null,
       cod_sub: null,
-
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-        },
-      },
-      series: [
-        {
-          name: "Vue Chart",
-          data: [30, 40, 45, 50, 49, 60, 70, 81],
-        },
-      ],
     };
   },
   methods: {
@@ -241,20 +226,6 @@ export default defineComponent({
       } catch (error) {
         console.log(error);
       }
-    },
-
-    updateChart() {
-      const max = 90;
-      const min = 20;
-      const newData = this.series[0].data.map(() => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-      });
-      // In the same way, update the series option
-      this.series = [
-        {
-          data: newData,
-        },
-      ];
     },
   },
 });
